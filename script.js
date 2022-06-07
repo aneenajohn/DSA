348756048511b679915157f8d7a2ab0495d68457
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2022-06-07 09:17:07 +0530

    Added script for finding nemo
    Signed-off-by: aneenajohn <75213145+aneenajohn@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const fishes = ["Nemo"];
 
+const findNemo = (array) => {
+  for (let i=0; i < array.length; i++) {
+    if (array[i].toLowerCase() ==="nemo") {
+      return console.log("Found Nemo!!!");
+    }
+    console.log("Nemo not found :(");
+  }
+};
+
+findNemo(fishes);
